875d5e6ace9a8a7d11081a0f35b9a4d56fe49199
sidharth5n <sidhunair5@gmail.com>
2021-04-03 13:02:54 +0530

    Removed discriminator forward pass

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch import optim
 from model import Audio2Keypoint
 from dataset import VoxKP
-from utils import KeyPointsRegLoss, keypoints_to_train, to_motion_delta
+from utils import KeyPointsRegLoss, keypoints_to_train
 
 data_loader =
 
@@ -25,9 +25,8 @@ iteration = 0
 
 for epoch in range(start_epoch, epochs):
     for data in data_loader:
-        image, audio, real_pose, fake_pose = [x.to(device) for x in data]
-        # TODO : Modify forward pass, image_enc_piv is not being used
-        image_enc_piv, fake_pose, real_enc, fake_enc = model(image, audio, real_pose, fake_pose)
+        image, audio, real_pose = [x.to(device) for x in data]
+        img_enc_piv, fake_pose, real_enc, fake_enc, real_pose_score, fake_pose_score = model(image, audio, real_pose)
 
         # remove base keypoint which is always [0,0]. Keeping it may ruin GANs training due discrete problems. etc.
         training_keypoints = self._get_training_keypoints()
@@ -39,43 +38,19 @@ for epoch in range(start_epoch, epochs):
         # Regression loss on motion or pose
         pose_regloss = keypts_regloss(train_real_pose, train_fake_pose)
 
-        # Global Discriminator and Hand Discriminator
-
-        # get full body keypoints
-        D_training_keypoints = self._get_training_keypoints()
-        D_real_pose = keypoints_to_train(real_pose, D_training_keypoints)
-        D_fake_pose = keypoints_to_train(fake_pose, D_training_keypoints)
-
-        # d motion or pose
-        if self.args.d_input == 'motion':
-            D_fake_pose_input = to_motion_delta(D_fake_pose)
-            D_real_pose_input = to_motion_delta(D_real_pose)
-        elif self.args.d_input == 'pose':
-            D_fake_pose_input = D_fake_pose
-            D_real_pose_input = D_real_pose
-        elif self.args.d_input == 'both':
-            # concatenate on the temporal axis
-            D_fake_pose_input = torch.cat([D_fake_pose, to_motion_delta(D_fake_pose)], dim = 1) # check dim
-            D_real_pose_input = torch.cat([D_real_pose, to_motion_delta(D_real_pose)], dim = 1) # check dim
-
-        # TODO : Move the discriminator part to model.py
-        fake_pose_score = self.discriminator(D_fake_pose_input)
-        real_pose_score = self.discriminator(D_real_pose_input)
-
-        # loss for training the global D
-        D_loss = Disc_loss(real_pose_score.new_ones(), real_pose_score) +
-                      args.lambda_d * Disc_loss(fake_pose_score.new_zeros(), fake_pose_score)
-        # train global D
+        # Compute discriminator loss and update D
+        D_loss_real = Disc_loss(real_pose_score.new_ones(), real_pose_score)
+        D_loss_fake = Disc_loss(fake_pose_score.new_zeros(), fake_pose_score)
+        D_loss = D_loss_real + args.lambda_d * D_loss_fake
         D_optim.zero_grad()
         D_loss.backward()
         D_optim.step()
 
         # Loss for training the generator from the global D - have I fooled the global D?
-        G_gan_loss = Disc_loss(fake_pose_score.new_ones(), fake_pose_score)
-
+        D_loss_fake = Disc_loss(fake_pose_score.new_ones(), fake_pose_score)
         # encoder loss and train encoder
         E_enc_loss = Enc_loss(img_enc_piv, real_enc, fake_enc)
-        E_loss = pose_regloss + G_gan_loss + E_enc_loss
+        E_loss = pose_regloss + D_loss_fake + E_enc_loss
         E_optim.zero_grad()
         E_loss.backward()
         E_optim.step()
@@ -83,7 +58,7 @@ for epoch in range(start_epoch, epochs):
         # loss for generatar encoding
         G_enc_loss = G_Enc_loss(img_enc_piv, fake_enc)
         # sum up ALL the losses for training the generator
-        G_loss = pose_regloss + (args.lambda_gan * G_gan_loss) + (args.lambda_enc * G_enc_loss)
+        G_loss = pose_regloss + (args.lambda_gan * D_loss_fake) + (args.lambda_enc * G_enc_loss)
         G_optim.zero_grad()
         G_loss.backward()
         G_optim.step()
