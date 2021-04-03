5aae4284e3b895e315c26918559ff1c885c22bad
sidharth5n <sidhunair5@gmail.com>
2021-04-03 13:02:37 +0530

    Included discriminator forward pass

--- model.py
+++ model.py
@@ -2,21 +2,31 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from layers import ImageEncoderPIV, Generator, Discriminator
+from utils import keypoints_to_train
 
 class Audio2Keypoint(nn.Module):
 
-    def __init__(self, seq_len = 64):
+    def __init__(self, args, seq_len = 64):
         super(Audio2Keypoint, self).__init__()
         self.encoder = ImageEncoderPIV()
         self.generator = Generator()
-        self.discriminator = Discriminator()
+        self.discriminator = Discriminator(args.d_input)
 
-    def forward(self, image, audio, real_pose, fake_pose):
+    def forward(self, image, audio, real_pose):
         img_enc_piv = self.encoder(image)
         fake_pose = self.generator(audio, real_pose, image, img_enc_piv)
-        real_enc = self.encoder(real_pose)
-        fake_enc = self.encoder(fake_pose)
-        return image_enc_piv, fake_pose, real_enc, fake_enc
+        if self.training:
+            # TODO : Check whether these encoder lines are required during inference
+            real_enc = self.encoder(real_pose)
+            fake_enc = self.encoder(fake_pose)
+            D_training_keypoints = self._get_training_keypoints() # get full body keypoints
+            D_real_pose = keypoints_to_train(real_pose, D_training_keypoints)
+            real_pose_score = self.discriminator(D_real_pose)
+            D_fake_pose = keypoints_to_train(fake_pose, D_training_keypoints)
+            fake_pose_score = self.discriminator(D_fake_pose)
+            return img_enc_piv, fake_pose, real_enc, fake_enc, real_pose_score, fake_pose_score
+        else:
+            return fake_pose
 
     def _get_training_keypoints(self):
         training_keypoints = []
