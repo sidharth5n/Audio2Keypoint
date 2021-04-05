438824ad1344430606d5e47d4997d23f81e2fb3c
sidharth5n <sidhunair5@gmail.com>
2021-04-05 08:37:41 +0530

    Updated keypoints

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from layers import ImageEncoderPIV, Generator, Discriminator
-from utils import keypoints_to_train
+from utils import keypoints_to_train, get_training_keypoints
 
 class Audio2Keypoint(nn.Module):
 
@@ -11,6 +11,7 @@ class Audio2Keypoint(nn.Module):
         self.encoder = ImageEncoderPIV()
         self.generator = Generator()
         self.discriminator = Discriminator(args.d_input)
+        self.register_buffer('keypoints', get_training_keypoints()) # get full body keypoints
 
     def forward(self, image, audio, real_pose):
         img_enc_piv = self.encoder(image)
@@ -19,25 +20,10 @@ class Audio2Keypoint(nn.Module):
             # TODO : Check whether these encoder lines are required during inference
             real_enc = self.encoder(real_pose)
             fake_enc = self.encoder(fake_pose)
-            D_training_keypoints = self._get_training_keypoints() # get full body keypoints
-            D_real_pose = keypoints_to_train(real_pose, D_training_keypoints)
+            D_real_pose = keypoints_to_train(real_pose, self.keypoints)
             real_pose_score = self.discriminator(D_real_pose)
-            D_fake_pose = keypoints_to_train(fake_pose, D_training_keypoints)
+            D_fake_pose = keypoints_to_train(fake_pose, self.keypoints)
             fake_pose_score = self.discriminator(D_fake_pose)
             return img_enc_piv, fake_pose, real_enc, fake_enc, real_pose_score, fake_pose_score
         else:
             return fake_pose
-
-    def _get_training_keypoints(self):
-        training_keypoints = []
-        training_keypoints.extend(CHIN_KEYPOINTS)
-        training_keypoints.extend(LEFT_BROW_KEYPOINTS)
-        training_keypoints.extend(RIGHT_BROW_KEYPOINTS)
-        training_keypoints.extend(NOSE_KEYPOINTS)
-        training_keypoints.extend(LEFT_EYE_KEYPOINTS)
-        training_keypoints.extend(RIGHT_EYE_KEYPOINTS)
-        training_keypoints.extend(OUTER_LIP_KEYPOINTS)
-        training_keypoints.extend(INNER_LIP_KEYPOINTS)
-
-        training_keypoints = sorted(list(set(training_keypoints)))
-        return training_keypoints                                                                                     var_list=trainable_variables)
