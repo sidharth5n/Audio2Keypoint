1373dae88904a9b8d09ee6493008208f3b7d0347
sidharth5n <sidhunair5@gmail.com>
2021-04-12 12:11:28 +0530

    Bug fixes

--- utils.py
+++ utils.py
@@ -5,21 +5,18 @@ import torchaudio.functional as F_au
 from common.consts import CHIN_KEYPOINTS, LEFT_BROW_KEYPOINTS, RIGHT_BROW_KEYPOINTS, NOSE_KEYPOINTS,\
     LEFT_EYE_KEYPOINTS, RIGHT_EYE_KEYPOINTS, OUTER_LIP_KEYPOINTS, INNER_LIP_KEYPOINTS
 
-def ConvLayer(in_channels, out_channels, kernel_size, stride, padding, type, norm = True, seq = False):
-    assert type in ['1D', '2D']
-    conv = nn.Conv1d if type == '1D' else nn.Conv2d
-    norm_ = nn.BatchNorm1d if type == '1D' else nn.BatchNorm2d
+def ConvLayer(in_channels, out_channels, kernel_size, stride, padding, conv_type, norm = True, seq = False):
+    assert conv_type in ['1D', '2D']
+    conv = nn.Conv1d if conv_type == '1D' else nn.Conv2d
+    norm_ = nn.BatchNorm1d if conv_type == '1D' else nn.BatchNorm2d
     layers = [conv(in_channels = in_channels,
                               out_channels = out_channels,
                               kernel_size = kernel_size,
                               stride = stride,
                               padding = padding),
-              norm_(out_channels) if norm else lambda x: x,
+              norm_(out_channels) if norm else nn.Identity(),
               nn.LeakyReLU(0.2)]
     return nn.Sequential(*layers) if seq else layers
-
-def CatAndAdd(x, y, layer):
-    return layer(torch.repeat_interleave(x, 2, dim = 2) + y)
 
 def MelSpectrogram(audio):
     """
@@ -46,26 +43,28 @@ def MelSpectrogram(audio):
 
 def keypoints_to_train(poses, arr):
     shape = poses.shape
-    reshaped = poses.view((shape[0], shape[1], 2, 68))
-    required_keypoints = torch.gather(reshaped, index = arr, dim = 3)
-    required_keypoints = required_keypoints.view((shape[0], shape[1], 2*len(arr)))
-    return required_keypoints
+    poses = poses.permute(0, 2, 1)
+    reshaped = poses.reshape((shape[0], shape[2], 2, 68))
+    # required_keypoints = torch.gather(reshaped, index = arr, dim = 3)
+    required_keypoints = reshaped[...,arr]
+    required_keypoints = required_keypoints.reshape((shape[0], shape[2], 2*len(arr)))
+    return required_keypoints.permute(0, 2, 1)
 
 def to_motion_delta(pose_batch):
     shape = pose_batch.shape
-    reshaped = pose_batch.view((-1, 64, 2, shape[-1]/2))
-    diff = reshaped[:, 1:] - reshaped[:, :-1]
-    diff = diff.view((-1, 63, shape[-1]))
+    reshaped = pose_batch.view((-1, 2, shape[1] // 2, 64))
+    diff = reshaped[..., 1:] - reshaped[..., :-1]
+    diff = diff.view((-1, shape[1], 63))
     return diff
 
 class KeyPointsRegLoss(nn.Module):
-    def __init__(self, type, loss_on, lambda_motion):
+    def __init__(self, type, loss_on, alpha_motion):
         super(KeyPointsRegLoss, self).__init__()
         assert type in ['L1', 'L2']
         self.loss = nn.L1Loss() if type == 'L1' else nn.MSELoss()
         assert loss_on in ['pose', 'motion', 'both']
         self.loss_on = loss_on
-        self.lambda = lambda_motion
+        self.alpha = alpha_motion
 
     def forward(self, real_keypts, fake_keypts):
         loss = 0
@@ -75,7 +74,7 @@ class KeyPointsRegLoss(nn.Module):
         if self.loss_on in ['motion', 'both']:
             real_keypts_motion = to_motion_delta(real_keypts).view(-1)
             fake_keypts_motion = to_motion_delta(fake_keypts).view(-1)
-            loss += self.loss(real_keypts_motion, fake_keypts_motion) * self.lambda
+            loss += self.loss(real_keypts_motion, fake_keypts_motion) * self.alpha
         return loss
 
 def get_training_keypoints():
