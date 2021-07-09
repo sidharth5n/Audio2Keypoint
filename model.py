ffaacf4b2f98f7525ba6b189eecaebe5e8e213a7
sidharth5n <sidhunair5@gmail.com>
2021-07-09 12:55:06 +0530

    Separate forward pass
    Added separate forward pass for training each component and for 
    validation.

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+
 from layers import ImageEncoderPIV, Generator, Discriminator
 from utils import keypoints_to_train, get_training_keypoints
 
@@ -13,39 +14,135 @@ class Audio2Keypoint(nn.Module):
         self.register_buffer('keypoints', get_training_keypoints()) # get full body keypoints
         self.__init_weights()
 
-    def forward(self, audio, real_pose):
+    def forward(self, *args, **kwargs):
+        mode = kwargs.get('mode', 'train_D')
+        if 'mode' in kwargs:
+            del kwargs['mode']
+        return getattr(self, '_' + mode)(*args, **kwargs)
+
+    def _train_D(self, audio_spect, real_pose):
         """
+        Forward pass for training discriminator.
+
         Parameters
         ----------
-        audio                    : torch.tensor of shape (B, config.input_shape[1])
-        real_pose                : torch.tensor of shape (B, 136, 64)
+        audio_spect     : torch.tensor of shape (B, 1, 418, 64)
+                          Mel spectrogram of audio
+        real_pose       : torch.tensor of shape (B, 136, 64)
+                          Ground truth pose
 
         Returns
         -------
-        img_enc_piv              : torch.tensor of shape (B, 32, 1)
-        fake_pose                : torch.tensor of shape (B, 136, 64)
-        real_enc                 : torch.tensor of shape (B, 32, 64)
-        fake_enc                 : torch.tensor of shape (B, 32, 64)
-        real_pose_score          : torch.tensor of shape (B, 16)
-        fake_pose_score          : torch.tensor of shape (B, 16)
-        fake_pose_score_detached : torch.tensor of shape (B, 16)
+        real_pose_score : torch.tensor of shape (B, 16)
+                          Realism score assigned by discriminator for real pose
+        fake_pose_score : torch.tensor of shape (B, 16)
+                          Realism score assigned by discriminator for fake pose
+        """
+        with torch.no_grad():
+            image = real_pose[...,0].unsqueeze(2) #(B, 136, 1)
+            img_enc_piv = self.encoder(image) #(B,32,1)
+            fake_pose = self.generator(audio_spect, real_pose, image, img_enc_piv) #(B,136,64)
+        D_real_pose = keypoints_to_train(real_pose, self.keypoints) #(B,134,64)
+        real_pose_score = self.discriminator(D_real_pose) #(B,16)
+        D_fake_pose = keypoints_to_train(fake_pose, self.keypoints)
+        fake_pose_score = self.discriminator(D_fake_pose)
+        return real_pose_score, fake_pose_score
+
+    def _train_G(self, audio_spect, real_pose):
+        """
+        Forward pass for training generator.
+
+        Parameters
+        ----------
+        audio_spect     : torch.tensor of shape (B, 1, 418, 64)
+                          Mel spectrogram of audio
+        real_pose       : torch.tensor of shape (B, 136, 64)
+                          Ground truth pose
+
+        Returns
+        -------
+        img_enc_piv     : torch.tensor of shape (B, 32)
+                          PIV encoding of input image
+        fake_pose       : torch.tensor of shape (B, 136, 64)
+                          Pose created by generator
+        real_enc        : torch.tensor of shape (B, 32)
+                          PIV encoding of real pose
+        fake_enc        : torch.tensor of shape (B, 32)
+                          PIV encoding of fake pose
+        fake_pose_score : torch.tensor of shape (B, 16)
+                          Realism score assigned by discriminator
         """
         image = real_pose[...,0].unsqueeze(2) #(B, 136, 1)
-        img_enc_piv = self.encoder(image) #(B,32,1)
-        fake_pose = self.generator(audio, real_pose, image, img_enc_piv) #(B,136,64)
-        if self.training:
-            # TODO : Check whether these encoder lines are required during inference
-            real_enc = self.encoder(real_pose) #(B,32,64)
-            fake_enc = self.encoder(fake_pose) #(B,32,64)
-            D_real_pose = keypoints_to_train(real_pose, self.keypoints) #(B,134,64)
-            real_pose_score = self.discriminator(D_real_pose) #(B,16)
+        with torch.no_grad():
+            img_enc_piv = self.encoder(image) #(B,32)
+        fake_pose = self.generator(audio_spect, real_pose, image, img_enc_piv) #(B,136,64)
+        real_enc = self.encoder(real_pose) #(B,32)
+        fake_enc = self.encoder(fake_pose) #(B,32)
+        D_fake_pose = keypoints_to_train(fake_pose, self.keypoints)
+        fake_pose_score = self.discriminator(D_fake_pose)
+        return img_enc_piv, fake_pose, real_enc, fake_enc, fake_pose_score
+
+    def _train_E(self, audio_spect, real_pose):
+        """
+        Forward pass for training PIV Encoder.
+
+        Parameters
+        ----------
+        audio_spect     : torch.tensor of shape (B, 1, 418, 64)
+                          Mel spectrogram of audio
+        real_pose       : torch.tensor of shape (B, 136, 64)
+                          Ground truth pose
+
+        Returns
+        -------
+        img_enc_piv     : torch.tensor of shape (B, 32)
+                          PIV encoding of input image
+        fake_pose       : torch.tensor of shape (B, 136, 64)
+                          Pose created by generator
+        real_enc        : torch.tensor of shape (B, 32)
+                          PIV encoding of real pose
+        fake_enc        : torch.tensor of shape (B, 32)
+                          PIV encoding of fake pose
+        fake_pose_score : torch.tensor of shape (B, 16)
+                          Realism score assigned by discriminator
+        """
+        image = real_pose[...,0].unsqueeze(2) #(B, 136, 1)
+        img_enc_piv = self.encoder(image) #(B,32)
+        fake_pose = self.generator(audio_spect, real_pose, image, img_enc_piv) #(B,136,64)
+        real_enc = self.encoder(real_pose) #(B,32)
+        fake_enc = self.encoder(fake_pose) #(B,32)
+        D_fake_pose = keypoints_to_train(fake_pose, self.keypoints)
+        fake_pose_score = self.discriminator(D_fake_pose)
+        return img_enc_piv, fake_pose, real_enc, fake_enc, fake_pose_score
+
+    def _predict(self, audio_spect, real_pose):
+        """
+        Forward pass for validation.
+
+        audio_spect     : torch.tensor of shape (B, 1, 418, 64)
+                          Mel spectrogram of audio
+        real_pose       : torch.tensor of shape (B, 136, 64)
+                          Ground truth pose
+
+        Returns
+        -------
+        fake_pose       : torch.tensor of shape (B, 136, 64)
+                          Pose created by generator
+        fake_pose_score : torch.tensor of shape (B, 16)
+                          Realism score assigned by discriminator
+        fake_enc        : torch.tensor of shape (B, 32)
+                          PIV encoding of fake pose
+        img_enc_piv     : torch.tensor of shape (B, 32)
+                          PIV encoding of input image
+        """
+        with torch.no_grad():
+            image = real_pose[...,0].unsqueeze(2) #(B, 136, 1)
+            img_enc_piv = self.encoder(image) #(B,32)
+            fake_pose = self.generator(audio_spect, real_pose, image, img_enc_piv) #(B,136,64)
+            fake_enc = self.encoder(fake_pose) #(B,32)
             D_fake_pose = keypoints_to_train(fake_pose, self.keypoints)
             fake_pose_score = self.discriminator(D_fake_pose)
-            D_fake_pose = keypoints_to_train(fake_pose.detach(), self.keypoints)
-            fake_pose_score_detached = self.discriminator(D_fake_pose)
-            return img_enc_piv, fake_pose, real_enc, fake_enc, real_pose_score, fake_pose_score, fake_pose_score_detached
-        else:
-            return fake_pose
+        return fake_pose, fake_pose_score, fake_enc, img_enc_piv
 
     def __init_weights(self):
         for p in self.parameters():
